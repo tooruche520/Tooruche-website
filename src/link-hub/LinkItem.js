7824bd8e42e315e596e4b4040bff89e11cc1a5ad
tooruche520 <tooruche520@gmail.com>
2023-05-18 09:43:40 +0800

    網站樣式更新
    新增連結背景圖
    修改游標樣式

--- src/link-hub/LinkItem.js
+++ src/link-hub/LinkItem.js
@@ -1,7 +1,13 @@
 import img_googleDrive from '../img/icon_GoogleDrive_.png';
 import img_youtube from '../img/icon_youtube_.png';
+import img_flickr from '../img/img_flickr.png';
+
 import img_googleForm from '../img/google_form.png';
-import img_tooruche from '../img/Tooru_3_squar.jpg'
+import img_tooruche from '../img/Tooru_3_squar.jpg';
+
+
+// import img_2023paroto from '../img/2023_paroto.jpg';
+
 
 import './LinkItem.scss';
 
@@ -9,12 +15,19 @@ function LinkItem(props) {
 	var title = props.item.title;
 	var description = props.item.description;
 	var url = props.item.url;
+	var backimage = props.item.backimage;
+
+	
+
+	// let img_ = require(backimage)
 
 	let img = url => {
 		if (url.includes('drive.google.com')) {
 			return img_googleDrive;
-		} else if (url.includes('www.youtube.com')) {
+		} else if (url.includes('youtube.com')) {
 			return img_youtube;
+		} else if (url.includes('flic.kr')) {
+			return img_flickr;
 		} else {
 			return img_tooruche;
 		}
@@ -28,7 +41,8 @@ function LinkItem(props) {
 			<p>
 				{description}
 			</p>
-			<img src={img(url)} width="35px" />
+			<img className="background" src={backimage}/>
+			<img className="icon" src={img(url)} width="35px" />
 		</a>
 	);
 }
